fb1db44b7c6487607fb386c5c1bc9c7abdcc962b
Nick <24507453+code1100@users.noreply.github.com>
2018-11-28 20:42:52 +0000

    Add files via upload

--- Software Programmng/Storage.Designer.cs
+++ Software Programmng/Storage.Designer.cs
@@ -29,11 +29,111 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.databaseDataSet4 = new Software_Programmng.DatabaseDataSet4();
+            this.storageBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.storageTableAdapter = new Software_Programmng.DatabaseDataSet4TableAdapters.StorageTableAdapter();
+            this.locationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contentsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maxSpaceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usedSpaceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.storageBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(625, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.locationDataGridViewTextBoxColumn,
+            this.contentsDataGridViewTextBoxColumn,
+            this.maxSpaceDataGridViewTextBoxColumn,
+            this.usedSpaceDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.storageBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(186, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // databaseDataSet4
+            // 
+            this.databaseDataSet4.DataSetName = "DatabaseDataSet4";
+            this.databaseDataSet4.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // storageBindingSource
+            // 
+            this.storageBindingSource.DataMember = "Storage";
+            this.storageBindingSource.DataSource = this.databaseDataSet4;
+            // 
+            // storageTableAdapter
+            // 
+            this.storageTableAdapter.ClearBeforeFill = true;
+            // 
+            // locationDataGridViewTextBoxColumn
+            // 
+            this.locationDataGridViewTextBoxColumn.DataPropertyName = "Location";
+            this.locationDataGridViewTextBoxColumn.HeaderText = "Location";
+            this.locationDataGridViewTextBoxColumn.Name = "locationDataGridViewTextBoxColumn";
+            // 
+            // contentsDataGridViewTextBoxColumn
+            // 
+            this.contentsDataGridViewTextBoxColumn.DataPropertyName = "Contents";
+            this.contentsDataGridViewTextBoxColumn.HeaderText = "Contents";
+            this.contentsDataGridViewTextBoxColumn.Name = "contentsDataGridViewTextBoxColumn";
+            // 
+            // maxSpaceDataGridViewTextBoxColumn
+            // 
+            this.maxSpaceDataGridViewTextBoxColumn.DataPropertyName = "MaxSpace";
+            this.maxSpaceDataGridViewTextBoxColumn.HeaderText = "MaxSpace";
+            this.maxSpaceDataGridViewTextBoxColumn.Name = "maxSpaceDataGridViewTextBoxColumn";
+            // 
+            // usedSpaceDataGridViewTextBoxColumn
+            // 
+            this.usedSpaceDataGridViewTextBoxColumn.DataPropertyName = "UsedSpace";
+            this.usedSpaceDataGridViewTextBoxColumn.HeaderText = "UsedSpace";
+            this.usedSpaceDataGridViewTextBoxColumn.Name = "usedSpaceDataGridViewTextBoxColumn";
+            // 
+            // Storage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Storage";
             this.Text = "Storage";
+            this.Load += new System.EventHandler(this.Storage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseDataSet4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.storageBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DatabaseDataSet4 databaseDataSet4;
+        private System.Windows.Forms.BindingSource storageBindingSource;
+        private DatabaseDataSet4TableAdapters.StorageTableAdapter storageTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn locationDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn contentsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maxSpaceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn usedSpaceDataGridViewTextBoxColumn;
     }
 }
--- Software Programmng/Timetable.Designer.cs
+++ Software Programmng/Timetable.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Timetable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Timetable";
             this.Text = "Timetable";
+            this.Load += new System.EventHandler(this.Timetable_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
