970333d00b3dcda444cc39a4410f82a9adec7df7
C-a-l-e-b <43847971+C-a-l-e-b@users.noreply.github.com>
2018-12-08 19:39:27 +0000

    Add files via upload

--- Software Programmng/Timetable.Designer.cs
+++ Software Programmng/Timetable.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.SuspendLayout();
             // 
             // button1
@@ -41,11 +42,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(2, 1);
+            this.monthCalendar1.Location = new System.Drawing.Point(117, 85);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
             // Timetable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.button1);
             this.Name = "Timetable";
             this.Text = "Timetable";
@@ -57,5 +68,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
     }
 }
--- Software Programmng/Timetable.cs
+++ Software Programmng/Timetable.cs
@@ -8,13 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Software_Programmng
 {
     public partial class Timetable : Form
     {
+
         public Timetable()
         {
             InitializeComponent();
+
+   
         }
 
         private void Timetable_Load(object sender, EventArgs e)
@@ -25,6 +29,19 @@ namespace Software_Programmng
         private void button1_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.OK;
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            DateTimePicker dateTimePicker1 = new DateTimePicker();
+            dateTimePicker1.MinDate = new DateTime(2018, 12, 1);
+            dateTimePicker1.MaxDate = new DateTime(2018, 12, 30);
+  
+           
+            MessageBox.Show("The Day of the week is " + dateTimePicker1.ToString());
+            
+              
+
         }
     }
 }
